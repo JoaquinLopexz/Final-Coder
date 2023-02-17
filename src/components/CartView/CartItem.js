7899173cb577d8adad6b6b7fcc87366b2ae717ac
Joaquino <joaquinlopezdottori@gmail.com>
2023-02-17 12:48:17 -0300

    Arreglado

--- src/components/CartView/CartItem.js
+++ src/components/CartView/CartItem.js
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./CartItem.css";
 
-const CartItem = ({producto}) => {
-  
-  
-  
-  
-  
-  
-  
-  
-    return (
-    <div>{producto.name}
-        : {producto.precio}    
+
+const CartItem = ({ producto }) => {
+  return (
+    <div className="card">
+     <img src={producto.imgUrl} alt='imgen de producto' />
+      {producto.name}
+      <b>Precio:</b> {producto.precio}
+      <b>
+        Cantidad:
+      </b>
     </div>
+  );
+};
 
-  )
-}
-
-export default CartItem
+export default CartItem;
